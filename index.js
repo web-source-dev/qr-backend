ec16f64f5c19dda7de7d75665046af4abd7c9f28
web-source-dev <muhammadnouman72321@gmail.com>
2024-11-12 14:04:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware for parsing JSON requests
-app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 // Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
